3229f465f5f2cdb966db8451d13364109989e323
CG360 <16578982+CloudG360@users.noreply.github.com>
2023-10-14 20:00:44 +0100

    project creation

---
+++ src/main/java/me/cg360/mod/bowspambegone/BowSpamBeGone.java
@@ -0,0 +1,10 @@
+package me.cg360.mod.bowspambegone;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BowSpamBeGone implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/cg360/mod/bowspambegone/client/BowSpamBeGoneClient.java
@@ -0,0 +1,12 @@
+package me.cg360.mod.bowspambegone.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BowSpamBeGoneClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
